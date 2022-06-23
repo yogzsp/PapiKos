d42630e3d5cff384f50525e4c62c6b99e2557bd9
Y5P <yogiyogi.prana43@gmail.com>
2022-06-23 22:59:43 +0800

    Aplikasi Papikos

---
+++ app/src/main/java/com/ithinkteam/papikos/MainActivity.java
@@ -0,0 +1,25 @@
+package com.ithinkteam.papikos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), Signin.class));
+                finish();
+            }
+        }, 1000L); //3000 L = 3 detik
+    }
+}
---
+++ app/src/main/java/com/ithinkteam/papikos/Signin.java
@@ -0,0 +1,22 @@
+package com.ithinkteam.papikos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Signin extends AppCompatActivity {
+
+//    TextView btn_masuk = findViewById(R.id.btn_masuk);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signin);
+
+    }
+}
