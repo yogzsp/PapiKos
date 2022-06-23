f14ceee231c4c5ddfd2c1ee88470d4570e7f992d
Y5P <yogiyogi.prana43@gmail.com>
2022-06-23 23:52:18 +0800

    Backend Update

--- app/src/main/java/com/ithinkteam/papikos/Signin.java
+++ app/src/main/java/com/ithinkteam/papikos/Signin.java
@@ -6,17 +6,113 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Signin extends AppCompatActivity {
 
-//    TextView btn_masuk = findViewById(R.id.btn_masuk);
+    TextView btn_masuk;
+    EditText et_email, et_password;
+    boolean v_email = false, v_pass = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signin);
 
+        btn_masuk = findViewById(R.id.btn_masuk);
+        btn_masuk.setEnabled(false);
+        btn_masuk.setBackgroundResource(R.color.hijau_tua);
+
+        et_email = findViewById(R.id.email);
+        et_password = findViewById(R.id.password);
+
+        //check input email
+        et_email.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String email = et_email.getText().toString();
+
+                if (checkEmail(email)) {
+                    v_email = true;
+                    if (v_email == true && v_pass == true) {
+                        btn_masuk.setEnabled(true);
+                    } else {
+                        btn_masuk.setEnabled(false);
+                        btn_masuk.setBackgroundResource(R.color.hijau_tua);
+                    }
+                } else {
+                    v_email = false;
+                    et_email.setError("Masukan email dengan benar!");
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        et_password.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String password = et_password.getText().toString();
+
+                if (checkPassword(password)) {
+                    if (password.length() < 8) {
+                        et_password.setError("Password harus terdiri dari 8 karakter!");
+                    } else {
+                        v_pass = true;
+                        if (v_email == true && v_pass == true) {
+                            btn_masuk.setEnabled(true);
+                        } else {
+                            btn_masuk.setEnabled(false);
+                            btn_masuk.setBackgroundResource(R.color.hijau_tua);
+                        }
+                    }
+                } else {
+                    v_pass = false;
+                    et_password.setError("Password harus terdiri dari huruf besar, kecil, angkat dan karakter spesial!");
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+
+    public static boolean checkPassword(String Password) {
+        String expression = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{4,}$";
+
+        Pattern pattern = Pattern.compile(expression);
+        Matcher matcher = pattern.matcher(Password);
+
+        return matcher.matches();
+    }
+
+    public static boolean checkEmail(String Email) {
+        String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+        Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(Email);
+        return matcher.matches();
     }
 }
