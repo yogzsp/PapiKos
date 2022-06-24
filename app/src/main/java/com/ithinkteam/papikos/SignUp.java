54e8bbd07b8cac32cb66a6752bd50714bd6efa79
Y5P <yogiyogi.prana43@gmail.com>
2022-06-24 12:06:43 +0800

    SignUP Perbarui

--- app/src/main/java/com/ithinkteam/papikos/SignUp.java
+++ app/src/main/java/com/ithinkteam/papikos/SignUp.java
@@ -2,13 +2,171 @@ package com.ithinkteam.papikos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SignUp extends AppCompatActivity {
+
+    TextView btn_masuk, btn_signin, errEmail, errPass,errUsername;
+    EditText et_email, et_password, et_username;
+    boolean v_email = false, v_pass = false,v_username=false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup);
+
+        btn_masuk = findViewById(R.id.btn_masuk);
+        btn_signin = findViewById(R.id.signIn);
+        errEmail = findViewById(R.id.errorEmail);
+        errPass = findViewById(R.id.errorPassword);
+        errUsername = findViewById(R.id.errorUsername);
+
+        btn_masuk.setEnabled(false);
+        btn_masuk.setBackgroundResource(R.color.hijau_tua);
+
+        et_email = findViewById(R.id.email);
+        et_username = findViewById(R.id.username);
+        et_password = findViewById(R.id.password);
+
+        //Klik Tombol
+        btn_masuk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Masih coming soon",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btn_signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Signin.class));
+            }
+        });
+
+
+        //check input
+        et_email.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String email = et_email.getText().toString();
+
+                if (checkEmail(email)) {
+                    v_email = true;
+                    errEmail.setVisibility(View.INVISIBLE);
+                } else {
+                    v_email = false;
+                    errEmail.setVisibility(View.VISIBLE);
+                }
+
+                checkValue();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        et_username.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String username = et_username.getText().toString();
+
+                if (username.length() >= 4) {
+                    v_username = true;
+                    errUsername.setVisibility(View.INVISIBLE);
+                } else {
+                    v_username = false;
+                    errUsername.setVisibility(View.VISIBLE);
+                }
+
+                checkValue();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        et_password.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String password = et_password.getText().toString();
+                if (checkPassword(password)) {
+                    if (password.length() < 8) {
+                        v_pass = false;
+                        errPass.setText("*password terdiri dari 8 karakter!");
+                        errPass.setVisibility(View.VISIBLE);
+                    } else {
+                        v_pass = true;
+                        errPass.setVisibility(View.INVISIBLE);
+                    }
+                } else {
+                    v_pass = false;
+                    errPass.setText("*password terdiri dari huruf besar,kecil, angka dan spesial!");
+                    errPass.setVisibility(View.VISIBLE);
+                }
+
+                checkValue();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+
+    public static boolean checkPassword(String Password) {
+        String expression = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{4,}$";
+
+        Pattern pattern = Pattern.compile(expression);
+        Matcher matcher = pattern.matcher(Password);
+
+        return matcher.matches();
+    }
+
+    public static boolean checkEmail(String Email) {
+        String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+        Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(Email);
+        return matcher.matches();
+    }
+
+    public void checkValue(){
+        if (v_email == true && v_pass == true && v_username == true) {
+            btn_masuk.setEnabled(true);
+            btn_masuk.setBackgroundResource(R.color.hijau);
+        } else {
+            btn_masuk.setEnabled(false);
+            btn_masuk.setBackgroundResource(R.color.hijau_tua);
+        }
     }
 }
